cd699e189d45699124f35d4e8b0428639cdf439f
Damian Arturo Garces Paez <guners86@hotmail.com>
2015-09-21 15:35:30 +0000

    Se agrega el modelo user y su respectiva vista

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  
+
   # Configuracion rutas questions
   root 'questions#index'
   resources :questions
+  
+   # Configuracion rutas users -> Solo se permite new y create 
+  resources :users, only: [:new, :create]
   
 end
