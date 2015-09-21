94bdef749be9a45993dc23a590dd46b4fd74b16a
Damian Arturo Garces Paez <guners86@hotmail.com>
2015-09-21 16:09:25 +0000

    Se agrega fuincionalidad de autenticar

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   # Configuracion rutas questions
   root 'questions#index'
   resources :questions
   
-   # Configuracion rutas users -> Solo se permite new y create 
+  # Configuracion rutas users -> Solo se permite new y create 
   resources :users, only: [:new, :create]
+  
+  # Configuracion rutas login new, create y logout
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
 end
