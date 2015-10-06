37e02dfccc3704d694984f950fdaf5c09b6c32f9
Damian Arturo Garces Paez <guners86@hotmail.com>
2015-10-06 00:10:42 +0000

    Se agrega validaciones al modelo user

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,9 @@ class User < ActiveRecord::Base
     has_many :comments
     has_many :votes
     
+    validates :email, uniqueness: true, format: /@/
+	validates :password, presence: true, on: :create
+	validates :password, length: { in: 6..20 }, allow_nil: true
+	validates :name, presence: true
+    
 end
